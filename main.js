3666b7004bae9e620a5c715f3b55e4947be9dd57
anitaok4 <114119009+anitaok4@users.noreply.github.com>
2024-04-22 20:59:27 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,45 @@
+img = "";
+objects = [];
+status = "";
+
+function preload(){
+    img = loadImage('dog_cat.jpg');
+}
+
+function setup(){
+    canvas = createCanvas(640, 420);
+    canvas.center();
+    objectDetector = ml5.objectDetector('cocossd', modelLoaded);
+    document.getElementById("status").innerHTML = "Status: Detectando Objetos";
+}
+
+function modelLoaded(){
+    console.log("Modelo Carregado!");
+    status = true;
+    objectDetector.detect(img, gotResult);
+}
+
+function gotResult(error, results){
+    if(error){
+        console.log(error);
+    }
+    console.log(results);
+    objects = results;
+}
+
+function draw(){
+    image(img, 0, 0, 640, 420);
+
+    if(status != ""){
+        for(i = 0; i < objects.length; i++){
+            document.getElementById("status").innerHTML = "Status: Objetos Detectados";
+
+            fill("#0a0800");
+            percent = floor(object[i].confidence * 100);
+            text(objects[i].label + "" + percent + "%", objects[i].x + 15, objects[i].y + 15);
+            noFill();
+            stroke("#105e8f");
+            rect(objects[i].x, objects[i].y, objects[i].width, objects[i].height);
+        }
+    }
+}
